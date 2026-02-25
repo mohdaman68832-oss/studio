b2ac7adc967d72b594beee9b8bb29e9bb9e358fa
MD Aman Saif <mohdaman68832@gmail.com>
2026-02-25 15:31:13 +0000

    Mujhe sirf bata do me kud kar lunga

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -4,19 +4,30 @@
 import { initializeApp, getApps, getApp, FirebaseApp } from 'firebase/app';
 import { getFirestore, Firestore } from 'firebase/firestore';
 import { getAuth, Auth } from 'firebase/auth';
+import { getAnalytics, isSupported, Analytics } from 'firebase/analytics';
 import { firebaseConfig } from './config';
 
 export function initializeFirebase(): {
   firebaseApp: FirebaseApp;
   firestore: Firestore;
   auth: Auth;
+  analytics?: Analytics;
 } {
   const firebaseApp =
     getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
   const firestore = getFirestore(firebaseApp);
   const auth = getAuth(firebaseApp);
+  
+  let analytics;
+  if (typeof window !== 'undefined') {
+    isSupported().then((supported) => {
+      if (supported) {
+        analytics = getAnalytics(firebaseApp);
+      }
+    });
+  }
 
-  return { firebaseApp, firestore, auth };
+  return { firebaseApp, firestore, auth, analytics };
 }
 
 export * from './provider';
