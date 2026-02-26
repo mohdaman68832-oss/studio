981e6ff9e4e6f70fae050966cf1a6620e1fa691d
MD Aman Saif <mohdaman68832@gmail.com>
2026-02-26 04:41:09 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,11 +1,8 @@
-
-'use client';
-
 export const firebaseConfig = {
-  apiKey: "placeholder-api-key",
-  authDomain: "placeholder-auth-domain",
-  projectId: "placeholder-project-id",
-  storageBucket: "placeholder-storage-bucket",
-  messagingSenderId: "placeholder-messaging-sender-id",
-  appId: "placeholder-app-id",
+  "projectId": "studio-4280353527-330eb",
+  "appId": "1:688774418858:web:846c2c1f6a865745ed9bd3",
+  "apiKey": "AIzaSyCJkJeo-70ArXkUWMO5GvKb8xMb0K1MlY4",
+  "authDomain": "studio-4280353527-330eb.firebaseapp.com",
+  "measurementId": "G-3Z8XK8DZSC",
+  "messagingSenderId": "688774418858"
 };
